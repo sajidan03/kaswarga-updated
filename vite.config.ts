7eac3eb7756f5d55565d0c5a895bc6b3138873f4
Sajidan03 <rifansyahsajidan03@gmail.com>
2025-09-06 17:13:43 +0700

    sajidan

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -13,10 +13,12 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
-        // wayfinder({
-        //     formVariants: true,
-        // }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
     esbuild: {
         jsx: 'automatic',
     },
